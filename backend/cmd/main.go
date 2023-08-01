46309ad55a416f39d4b130052e6ede8479015853
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-08-01 05:31:46 +0300

    make changes in connectToDB

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"log"
 
-	server "github.com/codescalersinternships/ToDoApp-Rodina/internal"
+	server "github.com/codescalersinternships/ToDoApp-Rodina/backend/internal"
+	"github.com/gin-gonic/gin"
 )
+
+// ErrDBPathNotFound is returned if the user doesn't mention the path of the database
+var ErrDBPathNotFound = errors.New("database path must be provided after the -db flag")
 
 func main() {
 
-	db, err := server.ConnectToDB("todoDB.db")
+	gin.SetMode(gin.ReleaseMode)
+
+	var dbPath string
+
+	flag.StringVar(&dbPath, "db", "./todoapp.db", "mention the filepath of the database")
+
+	flag.Parse()
+
+	if dbPath == "" {
+		log.Fatal(ErrDBPathNotFound)
+	}
+
+	db, err := server.ConnectToDB(dbPath)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	app := server.NewApp(db)
-
-	err = app.AppRouter()
+	app, err := server.NewApp(db)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if err = app.Run(); err != nil {
+
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 
 }
